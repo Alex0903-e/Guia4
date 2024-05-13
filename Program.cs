43ea823646cdaa1570068283e8dba884b8df9095
Alex0903-e <142626071+Alex0903-e@users.noreply.github.com>
2024-05-12 22:30:10 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Guia4;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+public partial class Universidad
+{
+    public static Main(String[] args)
+    {
+    //Lista de ALumnos
+    private List<Alumno> listaAlumnos;
+
+    //Constructor para iniciar la lista de alum
+    public Universidad()
+    {
+        List<Alumno> listaAlumnos = new List<Alumno>();
+
+    }
+
+    //Metodo estatico para agg a un alumno a la lista
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        listaAlumnos.Add(alumno);
+
+    }
+    //Metodo estatico para mostrar la lista de alumnos con detalles
+
+    public List<Alumno> ObtenerListaAlumnos()
+    {
+        return listaAlumnos;
+    }
+
+    public enum CarreraEst
+    {
+        Ingenieria_de_Sistemas,
+        Ingenieria_en_computacion,
+        Telecomunicaciones,
+        Ingenieria_Electronica,
+    }
+  
+ }  
+}
---
+++ Universidad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Guia4.CarreraEstudiante;
+
+namespace Guia4
+{
+   
+    
+        public class Universidad
+        {
+            public List<Alumno> Alumnos { get; set; }
+
+            public Universidad()
+            {
+                Alumnos = new List<Alumno>();
+                
+            }
+
+            public void AgregarAlumno(Alumno alumno)
+            {
+                Alumnos.Add(alumno);
+            }
+
+            public List<Alumno> ObtenerAlumnos()
+            {
+                return Alumnos;
+            }
+        }
+         public enum CarreraEst
+         {
+           Ingenieria_de_Sistemas,
+           Ingenieria_en_computacion,
+           Telecomunicaciones,
+           Ingenieria_Electronica,
+         }
+}
